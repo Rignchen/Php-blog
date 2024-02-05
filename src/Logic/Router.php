99e1a6bb5b1bc21bade06ab0301b2675008d5030
Rignchen <rinchendemort@gmail.com>
2024-02-05 14:51:08 +0100

    feat: add edit page

--- src/Logic/Router.php
+++ src/Logic/Router.php
@@ -16,6 +16,21 @@ class Router {
             $user = $db->get_user($args['username']);
             $data = $db->get_post($user['id'], $args['postName']);
             return $view->render($response, 'post.twig', [
+                'pageType' => 'post',
+                'data' => $data,
+                'creator' => $user,
+                'user' => $currentUser
+            ]);
+        });
+        $app->get('/edit/{username}/{postName}', function (Request $request, Response $response, $args) use ($db, $currentUser) {
+            $view = Twig::fromRequest($request);
+            $user = $db->get_user($args['username']);
+            $data = $db->get_post($user['id'], $args['postName']);
+            if ($currentUser['id'] !== $user['id']) {
+                return $response->withHeader('Location', '/post/' . $user['username'] . '/' . $data['title'])->withStatus(302);
+            }
+            return $view->render($response, 'post.twig', [
+                'pageType' => 'edit',
                 'data' => $data,
                 'creator' => $user,
                 'user' => $currentUser
